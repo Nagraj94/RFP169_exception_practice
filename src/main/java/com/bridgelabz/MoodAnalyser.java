a818b759bcb3c781c9dd2a7bf9615eb73a1a1782
Nagraj94 <ngowda335@gmail.com>
2022-08-04 19:31:11 +0530

    Mood analyser

---
+++ src/main/java/com/bridgelabz/MoodAnalyser.java
@@ -0,0 +1,20 @@
+package com.bridgelabz;
+
+public class MoodAnalyser {
+
+    public String analyseMood(String message){
+        if (message.contains("happy mood")) {
+            System.out.println("HAPPY");
+            return "HAPPY MOOD";
+        }
+        else {
+            System.out.println("SAD");
+            return "SAD MOOD";
+        }
+    }
+
+    public static void main(String[] args) {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        moodAnalyser.analyseMood("I'm in happy mood today");
+    }
+}
